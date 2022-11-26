d2c010c90099964378a2bd8bae7e37c00865d184
Norbert Hašan <99760438+bwooah@users.noreply.github.com>
2022-11-26 23:06:23 +0100

    zmena nazvu premennej

--- mapGen.py
+++ mapGen.py
@@ -1,16 +1,16 @@
 import random as rnd
 
-with open("test.txt", "w") as sxbao:
+with open("test.txt", "w") as f:
     for i in range(500):
         if i == 0:
             line = str(i)
             continue
         line += ',' + str(i)
-    sxbao.write(line+'\n')
+    f.write(line+'\n')
     for j in range(500):
         for k in range(500):
             if k == 0:
                 line = str(rnd.randrange(0, 200))
                 continue
             line += ',' + str(rnd.randrange(0, 200))
-        sxbao.write(line+'\n')
+        f.write(line+'\n')
