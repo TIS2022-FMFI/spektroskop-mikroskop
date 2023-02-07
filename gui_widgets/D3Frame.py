1c0940968be27598c4e8d9a751034434ff085889
Norbert Hašan <99760438+bwooah@users.noreply.github.com>
2023-02-07 21:03:28 +0100

    Add files via upload

--- gui_widgets/D3Frame.py
+++ gui_widgets/D3Frame.py
@@ -1,7 +1,6 @@
 from tkinter import *
 from gui_widgets.FrameBaseClass import FrameBaseClass
 from gui_widgets.SpectralMap3d import Render3DGraph
-from idlelib.tooltip import Hovertip
 
 
 class D3Frame(FrameBaseClass):
@@ -28,37 +27,27 @@ class D3Frame(FrameBaseClass):
         self.scanImage = Label(self, image=self.OFF_IMAGE, bg=self.FRAME_COLOR)
 
         # Entries
-        self.wavelengthEntry = self.initializeEntry(13)
+        self.wavelengthEntry = self.initializeEntry(15)
 
         # Entry values
         self.wavelengthValue = self.wavelengthEntry.get()
-
-        #Tooltips
-        self.wavelengthLabelTooltipMSG = "Image collum for image sequence analysis."
-        self.calibrationChartTooltip = Hovertip(self.wavelengthLabel,
-                                                       self.wavelengthLabelTooltipMSG)
 
         # Placing widgets into frame
         self.placeWidgets()
 
     def placeWidgets(self):
-        self.d3Label.pack()
-        self.wavelengthLabel.pack()
-        self.wavelengthEntry.pack()
-        self.showButton.pack(pady=(5,5))
-        self.scanLabel.pack()
-        self.scanImage.pack()
-
-
-
-        # self.d3Label.grid(sticky=W, row=0, column=0, pady=(10, 0))
-        # self.wavelengthLabel.grid(sticky=W, row=1, column=0, pady=(10, 0))
-        # self.wavelengthEntry.grid(sticky=W, row=1, column=1, padx=(0, 10), pady=(10, 0))
-        # self.showButton.grid(row=2, column=1, padx=(0, 10), pady=(10, 0))
-        # self.scanLabel.grid(sticky=W, row=3, column=0, padx=(0, 10), pady=(10, 10))
-        # self.scanImage.grid(row=3, column=0, padx=(30, 10), pady=(10, 10))
+        self.d3Label.grid(sticky=W, row=0, column=0, pady=(10, 0))
+        self.wavelengthLabel.grid(sticky=W, row=1, column=0, pady=(10, 0))
+        self.wavelengthEntry.grid(sticky=W, row=1, column=1, padx=(0, 10), pady=(10, 0))
+        self.showButton.grid(row=2, column=1, padx=(0, 10), pady=(10, 0))
+        self.scanLabel.grid(sticky=W, row=3, column=0, padx=(0, 10), pady=(10, 10))
+        self.scanImage.grid(row=3, column=0, padx=(30, 10), pady=(10, 10))
 
     def show3Dgraph(self):
         dataFor3D = self.motorFrame.motorController.dataContainer
         render = Render3DGraph(dataFor3D)
-        render.renderHeightMap(int(self.wavelengthEntry.get()))
+        value = self.wavelengthEntry.get()
+        if not value.isnumeric():
+            value = 0
+        render.renderHeightMap(int(value))
+        
